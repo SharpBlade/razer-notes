8387cec93cab9b8a80a38508149a8b3ba6134a88
Brandon Scott <brandon.scott03@gmail.com>
2013-08-29 21:46:14 +0100

    Added images for Dynamic Keys and implemented them

--- BrandonScott.RazerNotes/Windows/NotesWindow.xaml.cs
+++ BrandonScott.RazerNotes/Windows/NotesWindow.xaml.cs
@@ -15,9 +15,13 @@ namespace BrandonScott.RazerNotes.Windows
 
 #if RAZER
             SharpBladeHelper.Manager.Touchpad.SetWindow(this);
+            SharpBladeHelper.Manager.DynamicKeyEvent += Manager_DynamicKeyEvent;
+            SharpBladeHelper.Manager.EnableDynamicKey(Sharparam.SharpBlade.Native.RazerAPI.DynamicKeyType.DK1, NewClick, "C:\\Users\\Brandon\\Pictures\\RazerNotes\\RazerNotesAdd.png");
+            SharpBladeHelper.Manager.EnableDynamicKey(Sharparam.SharpBlade.Native.RazerAPI.DynamicKeyType.DK2, NewClick, "C:\\Users\\Brandon\\Pictures\\RazerNotes\\RazerNotesEdit.png");
+            SharpBladeHelper.Manager.EnableDynamicKey(Sharparam.SharpBlade.Native.RazerAPI.DynamicKeyType.DK3, NewClick, "C:\\Users\\Brandon\\Pictures\\RazerNotes\\RazerNotesDelete.png");
 #endif
         }
-
+          
         public NotesWindow(Note note) : this()
         {
             var notesVm = (NotesViewModel) DataContext;
@@ -29,6 +33,10 @@ namespace BrandonScott.RazerNotes.Windows
 
         private void NewClick(object sender, RoutedEventArgs e)
         {
+            NewNote();
+        }
+        private void NewNote()
+        {
 #if RAZER
             SharpBladeHelper.Manager.Touchpad.ClearWindow();
 #endif
@@ -39,24 +47,52 @@ namespace BrandonScott.RazerNotes.Windows
 
         private void DeleteButtonClick(object sender, RoutedEventArgs e)
         {
-            var selectedItem = NotesListBox.SelectedItem;
-
-            if (selectedItem == null)
-                return;
-
-            ((NotesViewModel) DataContext).Notes.Remove((Note) selectedItem);
+            DeleteNote();
         }
-
-        private void EditButtonClick(object sender, RoutedEventArgs e)
+        private void DeleteNote()
         {
             var selectedItem = NotesListBox.SelectedItem;
 
             if (selectedItem == null)
                 return;
 
-            Application.Current.MainWindow = new NoteWindow((Note) selectedItem);
+            ((NotesViewModel)DataContext).Notes.Remove((Note)selectedItem);
+        }
+        private void EditButtonClick(object sender, RoutedEventArgs e)
+        {
+            EditNote();
+        }
+        private void EditNote()
+        {
+            var selectedItem = NotesListBox.SelectedItem;
+
+            if (selectedItem == null)
+                return;
+
+            Application.Current.MainWindow = new NoteWindow((Note)selectedItem);
             Close();
             Application.Current.MainWindow.Show();
         }
+        private void ParameterFufill(object sender, System.EventArgs e)
+        {
+            //To fufill parameter
+        }
+
+        private void Manager_DynamicKeyEvent(object sender, Sharparam.SharpBlade.Razer.Events.DynamicKeyEventArgs e)
+        {
+            switch (e.KeyType)
+            {
+                case Sharparam.SharpBlade.Native.RazerAPI.DynamicKeyType.DK1:
+                    NewNote();
+                    break;
+                case Sharparam.SharpBlade.Native.RazerAPI.DynamicKeyType.DK2:
+                    EditNote();
+                    break;
+                case Sharparam.SharpBlade.Native.RazerAPI.DynamicKeyType.DK3:
+                    DeleteNote();
+                    break;
+            }
+        }
+
     }
 }
