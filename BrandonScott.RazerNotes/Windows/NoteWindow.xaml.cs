3b40ce7682aed86b204ac510cada6b0b78348de3
Brandon Scott <brandon.scott03@gmail.com>
2013-09-03 21:02:01 +0100

    Added it back

--- BrandonScott.RazerNotes/Windows/NoteWindow.xaml.cs
+++ BrandonScott.RazerNotes/Windows/NoteWindow.xaml.cs
@@ -15,6 +15,8 @@ namespace BrandonScott.RazerNotes.Windows
         private bool _clearTitle;
         private bool _clearContent;
 
+        private CaretManager _caretManager;
+
         public NoteWindow() : this(new Note("Tap to add note title", "Tap to add note content"))
         {
             _clearTitle = true;
@@ -36,7 +38,7 @@ namespace BrandonScott.RazerNotes.Windows
             SharpBladeHelper.Manager.DisableDynamicKey(RazerAPI.DynamicKeyType.DK3);
             SharpBladeHelper.Manager.DisableDynamicKey(RazerAPI.DynamicKeyType.DK5);
             SharpBladeHelper.Manager.DisableDynamicKey(RazerAPI.DynamicKeyType.DK10);
-            SharpBladeHelper.Manager.DynamicKeyEvent += OnDynamicKeyEvent;
+            SharpBladeHelper.Manager.DynamicKeyEvent += ManagerDynamicKeyEvent;
             SharpBladeHelper.Manager.EnableDynamicKey(RazerAPI.DynamicKeyType.DK1, @".\Resources\RazerNotesSave.png");
             SharpBladeHelper.Manager.EnableDynamicKey(RazerAPI.DynamicKeyType.DK2, @".\Resources\RazerNotesBack.png");
 
@@ -48,18 +50,31 @@ namespace BrandonScott.RazerNotes.Windows
 #endif               
         }
 
+        private void DisposeCaretManager()
+        {
+            if (_caretManager == null)
+                return;
+
+            _caretManager.Dispose();
+            _caretManager = null;
+        }
+
         private void SaveNote()
         {
+            DisposeCaretManager();
+
             _note.Title = NoteTitleBox.Text;
             _note.Content = NoteContentBox.Text;
+
 #if RAZER
             SharpBladeHelper.Manager.Touchpad.ClearWindow();
-            SharpBladeHelper.Manager.DynamicKeyEvent -= OnDynamicKeyEvent;
+            SharpBladeHelper.Manager.DynamicKeyEvent -= ManagerDynamicKeyEvent;
             SharpBladeHelper.Manager.SetKeyboardCapture(false);
             SharpBladeHelper.Manager.Touchpad.DisableGesture(RazerAPI.GestureType.Tap);
             SharpBladeHelper.Manager.Touchpad.Gesture -= TouchpadOnGesture;
             RenderPoll.Stop();
 #endif
+
             Application.Current.MainWindow = new NotesWindow(_note);
             Close();
             Application.Current.MainWindow.Show();           
@@ -67,32 +82,35 @@ namespace BrandonScott.RazerNotes.Windows
 
         private void Back()
         {
+            DisposeCaretManager();
+
 #if RAZER
             SharpBladeHelper.Manager.Touchpad.ClearWindow();
-            SharpBladeHelper.Manager.DynamicKeyEvent -= OnDynamicKeyEvent;
+            SharpBladeHelper.Manager.DynamicKeyEvent -= ManagerDynamicKeyEvent;
             SharpBladeHelper.Manager.SetKeyboardCapture(false);
             SharpBladeHelper.Manager.Touchpad.DisableGesture(RazerAPI.GestureType.Tap);
             SharpBladeHelper.Manager.Touchpad.Gesture -= TouchpadOnGesture;
             RenderPoll.Stop();
 #endif
+
             Application.Current.MainWindow = new NotesWindow();
             Close();
             Application.Current.MainWindow.Show();
         }   
 
-        private void OnDynamicKeyEvent(object sender, DynamicKeyEventArgs e)
+        private void ManagerDynamicKeyEvent(object sender, DynamicKeyEventArgs e)
         {
-            if (e.State == RazerAPI.DynamicKeyState.Down)
+            if (e.State != RazerAPI.DynamicKeyState.Down)
+                return;
+
+            switch (e.KeyType)
             {
-                switch (e.KeyType)
-                {
-                    case RazerAPI.DynamicKeyType.DK1:
-                        SaveNote();
-                        break;
-                    case RazerAPI.DynamicKeyType.DK2:
-                        Back();
-                        break;
-                }
+                case RazerAPI.DynamicKeyType.DK1:
+                    SaveNote();
+                    break;
+                case RazerAPI.DynamicKeyType.DK2:
+                    Back();
+                    break;
             }
         }
 
@@ -118,6 +136,7 @@ namespace BrandonScott.RazerNotes.Windows
                     NoteTitleBox.Clear();
                     _clearTitle = false;
                 }
+                _caretManager = new CaretManager(NoteTitleBox, NoteTitleBox.Text.Length);
                 HighlightInput(NoteTitleBox);
                 UnhighlightInput(NoteContentBox);
             }
@@ -130,23 +149,25 @@ namespace BrandonScott.RazerNotes.Windows
                     NoteContentBox.Clear();
                     _clearContent = false;
                 }
+                _caretManager = new CaretManager(NoteContentBox, NoteContentBox.Text.Length);
                 HighlightInput(NoteContentBox);
                 UnhighlightInput(NoteTitleBox);
             }
             else if (capturing)
             {
                 SharpBladeHelper.Manager.SetKeyboardCapture(false);
+                DisposeCaretManager();
                 UnhighlightInput(NoteContentBox);
                 UnhighlightInput(NoteTitleBox);
             }
         }
 
-        private static void HighlightInput(Control input)
+        private void HighlightInput(TextBox input)
         {
             input.BorderThickness = new Thickness(4, 4, 4, 4);
         }
 
-        private static void UnhighlightInput(Control input)
+        private void UnhighlightInput(TextBox input)
         {
             input.BorderThickness = new Thickness(2, 2, 2, 2);
         }
